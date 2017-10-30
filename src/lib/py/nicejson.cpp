0a89251e5b854539fed00414dcbdb8a0e2cfd7dd
Chet Murthy <chetsky@gmail.com>
2017-10-29 21:53:23 -0700

    more python baby-stepping

--- src/lib/py/nicejson.cpp
+++ src/lib/py/nicejson.cpp
@@ -4,16 +4,21 @@
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
 #include <boost/python/exception_translator.hpp>
+#include "NiceJSON.h"
 
 struct my_exception : std::exception
 {
   char const* what() const throw() { return "One of my exceptions"; }
 };
 
-void translate(my_exception const& e)
+void translate_my_exception(my_exception const& e)
 {
     // Use the Python 'C' API to set up an exception object
     PyErr_SetString(PyExc_Exception, e.what());
+}
+
+void translate_TException(apache::thrift::TException const& e) {
+  PyErr_SetString(PyExc_Exception, e.what());
 }
 
 void something_which_throws()
@@ -25,11 +30,17 @@ const std::string echo(const std::string name) {
     return std::string("hello, "+name);
 }
 
+void install_typelib(const std::string package, const std::string name, const string serialized) {
+  apache::thrift::nicejson::NiceJSON::install_typelib(package, name, serialized) ;
+}
+
 BOOST_PYTHON_MODULE(nicejson)
 {
   using namespace boost::python;
-  register_exception_translator<my_exception>(&translate);
+  register_exception_translator<my_exception>(&translate_my_exception);
+  register_exception_translator<apache::thrift::TException>(&translate_TException);
   
   def("something_which_throws", something_which_throws);
   def("echo", echo);
+  def("install_typelib", install_typelib);
 }
--- src/lib/py/setup.py
+++ src/lib/py/setup.py
@@ -1,3 +1,4 @@
+from os import environ
 from distutils.core import setup, Extension
  
 # define the name of the extension to use
@@ -8,7 +9,7 @@ extension_version = '1.0'
 # to get this to work, you may need to include the path
 # to your boost installation. Mine was in 
 # '/usr/local/include', hence the corresponding entry.
-include_dirs = [ '/usr/include' ]
+include_dirs = [ '/usr/include' , '../cpp' , environ['thrift_INCLUDEDIR'] ]
  
 # define the library directories to include any extra
 # libraries that may be needed.  The boost::python
@@ -22,4 +23,4 @@ libraries = [ 'boost_python' ]
 source_files = [ 'nicejson.cpp' ]
  
 # create the extension and add it to the python distribution
-setup( name=extension_name, version=extension_version, ext_modules=[Extension( extension_name, source_files, include_dirs=include_dirs, library_dirs=library_dirs, libraries=libraries )] )
+setup( name=extension_name, version=extension_version, ext_modules=[Extension( extension_name, source_files, include_dirs=include_dirs, library_dirs=library_dirs, libraries=libraries, extra_compile_args=['-std=gnu++11'] )] )
